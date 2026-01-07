ac9e0a5242589f120a322aee1d5204c86753ba53
praneethvelpumadugu-web <praneeth.velpumadugu@gmail.com>
2026-01-07 23:48:10 +0530

    Add comments to Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        // comments
         System.out.println("Hello SIT");
         System.out.println("Hello Java");
     }
